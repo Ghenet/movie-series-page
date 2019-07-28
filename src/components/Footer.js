c8efa758d31b97576e4b3b109a6e440cab681af9
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-07-27 20:54:08 -0700

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <div className='row'>
+                <div class='col-md-12'>
+                    <div class='footer p3 mt-4 text-center bg-dark text-light'>
+                        Developed By: 
+                        <span className='text-warning font-weight-normal'>
+                            Ghenet Kibrom
+                        </span>
+                        , Using <i className='fab fa-react' /> React Js  &amp; Redux JS integrated with external movies data API.
+                        <a href='http://www.omdbapi.com/' target='_blank'>OMDB</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
