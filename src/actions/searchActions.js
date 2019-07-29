a32182b5d88262e70511c9e827382693f04b33f3
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-07-28 21:18:24 -0700

    added a fetch movie action to display the details of a specific movie

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -1,4 +1,4 @@
-import { FETCH_MOVIE, SEARCH_MOVIE } from './types';
+import { FETCH_MOVIE, FETCH_MOVIES, SEARCH_MOVIE } from './types';
 import axios from 'axios';
 
 import {APIKey} from '../APIKey';
@@ -11,10 +11,19 @@ export const searchMovie = text => dispatch => {
 };
 
 export const fetchMovies = text => dispatch => {
-    axios.get(`http://www.omdbapi.com/?apikey=4495d8f7&s=${text}`)
+    // axios.get(`http://www.omdbapi.com/?apikey=4495d8f7&s=${text}`)
     .then(response => dispatch({
-        type: FETCH_MOVIE,
+        type: FETCH_MOVIES,
         payload: response.data.Search
     }))
     .catch(err => console.log(err))
 };
+
+export const fetchMovie = id => dispatch => {
+    // axios.get(`http://www.omdbapi.com/?apikey=495d8f7&i=${id}`)
+    .then(response =>
+        dispatch({
+            type: FETCH_MOVIE,
+            payload: response.data
+        })) 
+}
