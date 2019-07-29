c632742408da5409b60c2b698e8f43a3888d9813
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-07-28 19:53:48 -0700

    added a condition to display spinner or the movies container

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import SearchForm from './SearchForm';
-
+import Spinner from '../layout/Spinner';
+import MoviesContainer from './MoviesContainer';
 export class Landing extends Component {
     render() {
+//the following line esx structuring is saying we need to pull put loading out of props        
+        const {loading} = this.props;
         return (
             <div className='container'>
                 <SearchForm />
+                {loading ? <Spinner /> : <MoviesContainer />}
             </div>
         )
     }
 }
 
-export default Landing
+const mapStateToProps = state =>({
+    loading: state.movies.loading
+})
+
+export default connect(mapStateToProps)(Landing);
